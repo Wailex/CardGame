74a71874524bdfbf73db509350e8e8fa33544882
Wailex <mwag0654@740s-pl-103.local>
2016-10-25 09:22:24 -0600

    Created a new swift project Card Game.

---
+++ CardGame/Model/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  CardGame
+//
+//  Created by Wagner, Mason on 10/25/16.
+//  Copyright © 2016 Ctec. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Wagner, Mason on 10/25/16.
+//  Copyright © 2016 Ctec. All rights reserved.
+//
+
+import Foundation
